e3f537e6fb46501c79c8928d775dc6aed2eecfbe
Peter Oberlaender <pete@wayoutofline.net>
2024-10-07 16:43:53 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,22 +1,52 @@
+let projects = [
+    {
+        id: 1, name: 'Project 1', description: 'Description 1...', images: [
+            { id: 1, src: 'img/project1-1.jpg' },
+            { id: 2, src: 'https://placebear.com/600/400' },
+            { id: 3, src: 'https://placebear.com/600/400' },
+        ]
+    },
+    {
+        id: 2, name: 'Project 2', description: 'Description 2', images: [
+            { id: 1, src: 'img/project2-1.jpg' },
+            { id: 3, src: 'https://placebear.com/600/400' },
+        ]
+    },
+    {
+        id: 3, name: 'Project 3', description: 'Description 3', images: [
+            { id: 1, src: 'img/project3-1.jpg' },
+            { id: 2, src: 'https://placebear.com/600/400' },
+            { id: 3, src: 'https://placebear.com/600/400' },
+            { id: 3, src: 'https://placebear.com/600/400' },
+            { id: 3, src: 'https://placebear.com/600/400' },
+        ]
+    },
+    {
+        id: 3, name: 'Project 4', description: 'Description 4', images: [
+            { id: 1, src: 'https://placebear.com/600/400' },
+            { id: 2, src: 'https://placebear.com/600/400' },
+        ]
+    },
+]
+
+const Project = ({ project }) => {
+    return <div>
+        <h2>{project.name}</h2>
+        <p>{project.description}</p>
+        <div>
+            {project.images.map(image => (
+                <img className="project-img" key={image.id} src={image.src} alt={project.name} />
+            ))}
+        </div>
+    </div>
+}
+
 function MyApp() {
     return (
         <>
-            <figure>
-                <img src="https://www.placebear.com/300/300" alt=""></img>
-                <figcaption>Lorem ipsum dolor sit.</figcaption>
-            </figure>
-            <figure>
-                <img src="https://www.placebear.com/300/300" alt=""></img>
-                <figcaption>Lorem ipsum dolor sit.</figcaption>
-            </figure>
-            <figure>
-                <img src="https://www.placebear.com/300/300" alt=""></img>
-                <figcaption>Lorem ipsum dolor sit.</figcaption>
-            </figure>
-            <figure>
-                <img src="https://www.placebear.com/300/300" alt=""></img>
-                <figcaption>Lorem ipsum dolor sit.</figcaption>
-            </figure>
+            {projects.map(project => (
+                <Project key={project.id} project={project} />
+            ))}
         </>
     );
 }
