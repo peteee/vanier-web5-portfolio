15b24ecdd3ba02357e36a45eb5d62fbc1f66a479
Peter Oberlaender <pete@wayoutofline.net>
2024-10-07 15:19:18 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -24,3 +24,23 @@ function MyApp() {
 const container = document.getElementById('projects');
 const root = ReactDOM.createRoot(container);
 root.render(<MyApp />);
+
+
+
+const myMenu = document.querySelector("#menu");
+const myPanel = document.querySelector("#panel");
+const myCloseBtn = document.querySelector("#close");
+const mobileLinks = document.querySelectorAll("#panel nav a");
+
+myMenu.addEventListener("click", function() {
+    myPanel.classList.add("slide-in");
+});
+myCloseBtn.addEventListener("click", function() {
+    myPanel.classList.remove("slide-in");
+});
+
+mobileLinks.forEach(function(item) {
+    item.addEventListener("click", function() {
+        myPanel.classList.toggle("slide-in");
+    });
+});
