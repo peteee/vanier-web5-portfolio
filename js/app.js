da12374bf5fd66aeb100ac78ba9796e05cfeb909
Peter Oberlaender <pete@wayoutofline.net>
2024-10-09 16:14:13 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,41 +1,65 @@
 let projects = [
     {
-        id: 1, name: 'Project 1', description: 'Description 1...', images: [
+        id: 1, name: 'Project 1', enable: true, description: 'Description 1...', images: [
             { id: 1, src: 'img/project1-1.jpg' },
             { id: 2, src: 'https://placebear.com/600/400' },
             { id: 3, src: 'https://placebear.com/600/400' },
         ]
     },
     {
-        id: 2, name: 'Project 2', description: 'Description 2', images: [
+        id: 2, name: 'Project 2', enable: true, description: 'Description 2', images: [
             { id: 1, src: 'img/project2-1.jpg' },
-            { id: 3, src: 'https://placebear.com/600/400' },
+            { id: 2, src: 'https://placebear.com/600/400' },
         ]
     },
     {
-        id: 3, name: 'Project 3', description: 'Description 3', images: [
+        id: 3, name: 'Project 3', enable: true, description: 'Description 3', images: [
             { id: 1, src: 'img/project3-1.jpg' },
             { id: 2, src: 'https://placebear.com/600/400' },
             { id: 3, src: 'https://placebear.com/600/400' },
-            { id: 3, src: 'https://placebear.com/600/400' },
-            { id: 3, src: 'https://placebear.com/600/400' },
+            { id: 4, src: 'https://placebear.com/600/400' },
+            { id: 5, src: 'https://placebear.com/600/400' },
         ]
     },
     {
-        id: 3, name: 'Project 4', description: 'Description 4', images: [
+        id: 4, name: 'Project 4', enable: true, description: 'Description 4', images: [
             { id: 1, src: 'https://placebear.com/600/400' },
             { id: 2, src: 'https://placebear.com/600/400' },
         ]
     },
 ]
 
+const overLay = document.getElementById("overlay");
+
+function closeOverlay() {
+    overLay.style.opacity = "0";
+    overLay.classList.remove("show");
+    overLay.innerHTML = '';
+}
+
+function openOverlay(project) {
+    overLay.classList.add("show");
+    setTimeout(() => { overLay.style.opacity = "1" }, 300);
+    
+    //window.scrollTo(0, 0);
+
+    overLay.innerHTML = `<p class="close" onclick="closeOverlay()">x</p><h2>${project.name}</h2>
+    <p>${project.description}</p>`;
+
+    project.images.forEach((item, index) => {
+        overLay.innerHTML += `<img class="project-img-detail" src="${item.src}">`
+    })
+
+}
+
 const Project = ({ project }) => {
     return <div>
         <h2>{project.name}</h2>
+        {/* <p>{project.enable ? "project active" : "project inactive"}</p> */}
         <p>{project.description}</p>
         <div>
             {project.images.map(image => (
-                <img className="project-img" key={image.id} src={image.src} alt={project.name} />
+                <img className="project-img" key={image.id} src={image.src} alt={project.name} onClick={() => openOverlay(project)} />
             ))}
         </div>
     </div>
