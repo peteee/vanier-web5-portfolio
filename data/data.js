0fb5cf17139e00b907b517115eb02f45045aaab1
Peter Oberlaender <pete@wayoutofline.net>
2024-10-25 11:53:31 -0400

    Create data.js

---
+++ data/data.js
@@ -0,0 +1,30 @@
+let projects = [
+    {
+        id: 1, name: 'Project 1', enable: true, description: 'Description 1...', images: [
+            { id: 1, src: 'img/project1-1.jpg' },
+            { id: 2, src: 'https://placebear.com/600/400' },
+            { id: 3, src: 'https://placebear.com/600/400' },
+        ]
+    },
+    {
+        id: 2, name: 'Project 2', enable: false, description: 'Description 2', images: [
+            { id: 1, src: 'img/project2-1.jpg' },
+            { id: 2, src: 'https://placebear.com/600/400' },
+        ]
+    },
+    {
+        id: 3, name: 'Project 3', enable: true, description: 'Description 3', images: [
+            { id: 1, src: 'img/project3-1.jpg' },
+            { id: 2, src: 'https://placebear.com/600/400' },
+            { id: 3, src: 'https://placebear.com/600/400' },
+            { id: 4, src: 'https://placebear.com/600/400' },
+            { id: 5, src: 'https://placebear.com/600/400' },
+        ]
+    },
+    {
+        id: 4, name: 'Project 4', enable: true, description: 'Description 4', images: [
+            { id: 1, src: 'https://placebear.com/600/400' },
+            { id: 2, src: 'https://placebear.com/600/400' },
+        ]
+    },
+]
